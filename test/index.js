bb14c3a1d03dbeae70373060258aa5a60d7c1f73
Roland Warmerdam <roland@segment.com>
2018-03-03 11:05:21 -0800

    Debug test

--- test/index.js
+++ test/index.js
@@ -3,6 +3,27 @@ import Benchmark from '..'
 
 test('runs benchmark', async t => {
   const benchmark = new Benchmark()
+
+  benchmark.on('webpack', () => {
+    console.log('Compiling bundle')
+  })
+
+  benchmark.on('server', () => {
+    console.log('Starting server')
+  })
+
+  benchmark.on('chrome', () => {
+    console.log('Starting Chrome')
+  })
+
+  benchmark.on('start', () => {
+    console.log('Starting benchmark')
+  })
+
+  benchmark.on('progress', benchmark => {
+    console.log('run', benchmark.stats.sample.length)
+  })
+
   const result = await benchmark.run('test/fixtures/benchmark.js')
   t.truthy(result.stats)
   t.truthy(result.times)
