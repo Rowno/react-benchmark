ef2eb8da9563c2dfbbfd332e2dcd74da21d2abfb
Alexander Gusman <alex@goooseman.ru>
2022-07-31 08:25:56 +0300

    Enable CPU throttling with --cpuThrottle flag (#30)
    Looks good, thanks.

--- test/cli.js
+++ test/cli.js
@@ -10,3 +10,22 @@ test('runs benchmark', async (t) => {
 
   t.regex(result.stdout, /[0-9,]+ ops\/sec ±[0-9.]+% \(\d+ runs sampled\)/)
 })
+
+test('throttles CPU', async (t) => {
+  const getOpsSec = (resultString) => {
+    return parseInt(
+      resultString.match(/([\d,]+) ops\/sec/)[1].replace(/,/g, '')
+    )
+  }
+  const binPath = path.resolve(__dirname, '../lib/cli.js')
+  const fixturePath = path.resolve(__dirname, 'fixtures/benchmark.js')
+
+  const woutT = (await execa(binPath, [fixturePath])).stdout
+  const withT = (await execa(binPath, [fixturePath, '--cpuThrottle=4'])).stdout
+
+  // difference should be more then 2 times
+  t.assert(
+    getOpsSec(woutT) - getOpsSec(withT) > getOpsSec(woutT) / 2,
+    'The difference between throttled and not throttled execution is less then normal'
+  )
+})
